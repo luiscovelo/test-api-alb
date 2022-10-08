7ad2718c7bb9e045827e1ee8bc2d2bc738227007
Luis Felipe Silva Covelo <luis.covelo@pdsolucoes.com.br>
2022-10-08 10:18:37 -0300

    new version

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	api := gin.New()
+	gin.SetMode(gin.ReleaseMode)
+
+	api := gin.Default()
 	api.GET("/", handle)
+	api.GET("/hostname", hostname)
+	api.GET("/addr", localaddr)
 
 	err := api.Run(":80")
 
@@ -21,4 +28,31 @@ func main() {
 
 func handle(c *gin.Context) {
 	c.JSON(http.StatusOK, c.Request.Host)
+}
+
+func hostname(c *gin.Context) {
+
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, hostname)
+
+}
+
+func localaddr(c *gin.Context) {
+
+	addr := c.Request.Context().Value(http.LocalAddrContextKey)
+	host, port, err := net.SplitHostPort(fmt.Sprintf("%v", addr))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, host+":"+port)
+
 }
